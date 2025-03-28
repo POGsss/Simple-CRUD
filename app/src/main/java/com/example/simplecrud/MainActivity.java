028354dac2f5bbfb58190e199649e7c981cce7fb
POGsss <frankiepinat14@gmail.com>
2025-03-29 00:39:47 +0800

    Added App Icon

--- app/src/main/java/com/example/simplecrud/MainActivity.java
+++ app/src/main/java/com/example/simplecrud/MainActivity.java
@@ -3,12 +3,10 @@ package com.example.simplecrud;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SearchView;
-
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.FirebaseDatabase;
@@ -45,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         // Firebase
         FirebaseRecyclerOptions<MainModel> options =
                 new FirebaseRecyclerOptions.Builder<MainModel>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("data"), MainModel.class)
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("data").orderByChild(""), MainModel.class)
                         .build();
 
         mainAdapter = new MainAdapter(options);
